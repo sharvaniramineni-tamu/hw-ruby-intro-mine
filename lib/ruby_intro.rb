2749179a71f9b3290fd2e828115ccd6d35f0c93e
sharvaniramineni-tamu <sharvani.ramineni@tamu.edu>
2023-09-11 23:51:24 -0500

    max_2_sum is passing all tests

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -13,11 +13,28 @@ def sum(arr)
   sum = 0
   arr.each { |x| sum = sum + x}
   return sum
-  
+
 end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+  #calculate length of array
+  len = arr.length
+
+  # if array is empty return 0
+  # if array has only one element, then return that element
+  if len <= 0 
+    return 0
+  elsif len == 1
+    return arr[0]
+  end
+
+  # sort the array and reverse in descending order 
+  # and sum first two elements which are largest among others
+  sum = 0
+  new_arr = arr.sort.reverse
+  sum = new_arr[0] + new_arr[1]
+  return sum
 end
 
 def sum_to_n?(arr, number)
